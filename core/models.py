6a9e8a4de4aab492c5340ca0646d1bd211b89dac
Artem <mrshelkinpro228gmail.com>
2022-10-20 13:16:37 +0500

    add model struct

--- core/models.py
+++ core/models.py
@@ -1,3 +1,38 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
-# Create your models here.
+class Flower(models.Model):
+    title = models.CharField('Название', max_length=100)
+    description = models.TextField('Описание')
+    image = models.ImageField('Изображение', upload_to='flower/', validators=[FileExtensionValidator(allowed_extensions=['png', 'jpg'])])
+    price = models.FloatField('Цена', default=0)
+    in_stock = models.BooleanField('В наличии', default=False)
+
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Цветок'
+        verbose_name_plural = 'Цветы'
+
+class Bouquet(models.Model):
+    title = models.CharField('Название', max_length=100)
+    description = models.TextField('Описание')
+    image = models.ImageField('Изображение', upload_to='bouquet/', validators=[FileExtensionValidator(allowed_extensions=['png', 'jpg'])])
+    composition = models.ManyToManyField(to=Flower, verbose_name='Состав букета', related_name='bouquet_composition')
+    in_stock = models.BooleanField('В наличии',default=False)
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Букет'
+        verbose_name_plural = 'Букеты'
+
